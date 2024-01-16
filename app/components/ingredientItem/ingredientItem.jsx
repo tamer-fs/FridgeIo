a968a42c0f0bcfe203e8697108e9cf3a20733773
tamer-fs <113843535+tamer-fs@users.noreply.github.com>
2024-01-16 17:42:23 +0100

    fix

--- app/components/ingredientItem/ingredientItem.jsx
+++ app/components/ingredientItem/ingredientItem.jsx
@@ -6,7 +6,7 @@ export default function IngredientItem({ removeIngredient, name }) {
       {name}
       <img
         onClick={() => removeIngredient(name)}
-        src="close-fill.png"
+        src="./close-fill.png"
         alt="delete"
       ></img>
     </div>
