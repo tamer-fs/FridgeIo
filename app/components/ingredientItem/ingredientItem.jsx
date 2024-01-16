8a8ec7cfef72db115b22ef1c3fea8aa2f3d7657f
tamer-fs <113843535+tamer-fs@users.noreply.github.com>
2024-01-16 17:44:28 +0100

    fix2

--- app/components/ingredientItem/ingredientItem.jsx
+++ app/components/ingredientItem/ingredientItem.jsx
@@ -4,11 +4,10 @@ export default function IngredientItem({ removeIngredient, name }) {
   return (
     <div className="container">
       {name}
-      <img
-        onClick={() => removeIngredient(name)}
-        src="./close-fill.png"
-        alt="delete"
-      ></img>
+
+      <button onClick={() => removeIngredient(name)}>
+        <img src="./close-fill.png" alt="delete" />
+      </button>
     </div>
   );
 }
